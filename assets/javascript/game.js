70a886cb8106c327dc3b77b1f84395dcfdd47263
sandond77 <sandu@ucdavis.edu>
2017-11-16 01:00:24 -0800

    fixed bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,36 +9,40 @@ var score = 0;
 var flag = false;
 
 //this will prevent the gems from having the same values
-if (gemValue1 === gemValue2 || gemValue1 === gemValue3 || gemValue1 === gemValue4 || gemValue2 === gemValue3 || gemValue2 === gemValue4 || gemValue3 === gemValue4){
-	gemValue1 = Math.floor(Math.random()*12)+1;
-	gemValue2 = Math.floor(Math.random()*12)+1;
-	gemValue3 = Math.floor(Math.random()*12)+1;
-	gemValue4 = Math.floor(Math.random()*12)+1;
+function repeat() {
+	while (gemValue1 === gemValue2 || gemValue1 === gemValue3 || gemValue1 === gemValue4 || gemValue2 === gemValue3 || gemValue2 === gemValue4 || gemValue3 === gemValue4){
+		gemValue1 = Math.floor(Math.random()*12)+1;
+		gemValue2 = Math.floor(Math.random()*12)+1;
+		gemValue3 = Math.floor(Math.random()*12)+1;
+		gemValue4 = Math.floor(Math.random()*12)+1;
+	}
 }
 
-
-
 //assigns each button its gem value
-if ($("button").hasClass("gem1")===true){
-	$(".gem1").attr("value",gemValue1);
-} 
+function assign (){
+	if ($("button").hasClass("gem1")===true){
+		$(".gem1").attr("value",gemValue1);
+	} 
 
-if ($("button").hasClass("gem2")===true){
-	$(".gem2").attr("value",gemValue2);
-} 
+	if ($("button").hasClass("gem2")===true){
+		$(".gem2").attr("value",gemValue2);
+	} 
 
-if ($("button").hasClass("gem3")===true){
-	$(".gem3").attr("value",gemValue3);
-} 
+	if ($("button").hasClass("gem3")===true){
+		$(".gem3").attr("value",gemValue3);
+	} 
 
-if ($("button").hasClass("gem4")===true){
-	$(".gem4").attr("value",gemValue4);
+	if ($("button").hasClass("gem4")===true){
+		$(".gem4").attr("value",gemValue4);
+	}
 }
 
 
 //preloads the game when the page is loaded
+repeat();
+assign();
 $("#randomnumber").append("Target Score: " + randomNumber);
-$("#yourscore").append("Your Total Score: " + score);
+$("#yourscore").append("Your Score: " + score);
 $("#wins").append("Wins: " + wins);
 $("#losses").append("Losses: " + losses);
 
@@ -46,8 +50,9 @@ $("#losses").append("Losses: " + losses);
 $("button").click(function(){
 	if (flag === false){
 		score = score + parseInt(this.value);
+		console.log(this.value);
 		$("#yourscore").empty();
-		$("#yourscore").append("Your Total Score: " + score);
+		$("#yourscore").append("Your Score: " + score);
 
 		if (score === randomNumber){
 			alert("You WIN! Hit the reset button to play again!");
@@ -76,14 +81,16 @@ $(".reset").click(function(){
 	randomNumber = Math.floor(Math.random()*(100+1)) + 19;
 	$("#randomnumber").empty();
 	$("#randomnumber").append("Target Score: " + randomNumber);
+
 	gemValue1 = Math.floor(Math.random()*12)+1;
 	gemValue2 = Math.floor(Math.random()*12)+1;
 	gemValue3 = Math.floor(Math.random()*12)+1;
 	gemValue4 = Math.floor(Math.random()*12)+1;
+	repeat();
+	assign();
 	score = 0;
 	$("#yourscore").empty();
-	$("#yourscore").append("Your Total Score: " + score);
+	$("#yourscore").append("Your Score: " + score);
 	flag = false;
-
 })
 
